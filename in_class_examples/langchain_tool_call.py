053bdb6837bc76ea0ba92bab2fdb139e7b248641
ashtilawat23 <ashtilawat23@gmail.com>
2024-06-03 14:54:36 -0400

    examples added

---
+++ in_class_examples/langchain_tool_call.py
@@ -0,0 +1,20 @@
+import requests
+from langchain_openai import ChatOpenAI
+from langchain_core.tools import tool
+
+@tool # annotation lets Langchain to convert function to OpenAI function calling structure
+def fetch_weather(city: str):
+    """Gets the weather for a given city""" # required docstring
+    url = f"https://api.weatherapi.com/v1/current.json?key=e9bdc46fc5284476b6e123517241604&q={city}"
+    response = requests.get(url)
+    return response.json()['current']['condition']['text']
+
+llm = ChatOpenAI(model="gpt-3.5-turbo-0125")
+llm_with_tools = llm.bind_tools([fetch_weather]) 
+query = "What's the weather like in Salt Lake City?"
+result = llm_with_tools.invoke(query).tool_calls
+city = result[0]["args"]["city"]
+
+weather = fetch_weather(city)
+# Could also use the LLM to create a response with the findings
+print(f"The current weather in {city} is {weather}.") 
